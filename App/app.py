83a596d9d37e28217895c88cd5b5fc57fbeda08c
myriiioliveira <myriiioliveira@gmail.com>
2024-11-15 19:07:18 -0300

    Update app.py

--- App/app.py
+++ App/app.py
@@ -5,12 +5,12 @@ import numpy as np
 st.title("Web App Football Data")
 
 st.sidebar.header("Leagues")
-selected_league=st.sidebar.selectbox ('League', ['England', 'Germany', 'Italy', 'Spain', 'France', 'Scotland', 'Netherlands', 'Belgium', 'Portugal', 'Turkey', 'Greece', 'Argentina', 'Austria', 'Brazil', 'China', 'Denmark', 'Finland', 'Ireland', 'Japan', 'Mexico', 'Norway', 'Poland', 'Romania', 'Russia', 'Sweden', 'Switzerland', 'USA'])
+selected_league = st.sidebar.selectbox('League', ['England','Germany','Italy','Spain','France','Scotland','Netherlands','Belgium','Portugal','Turkey','Greece','Argentina','Austria','Brazil','China','Denmark','Finland','Ireland','Japan','Mexico','Norway','Poland','Romania','Russia','Sweden','Switzerland','USA'])
 
 st.sidebar.header("Season")
-selected_season=st.sidebar.selectbox ('Season", ['2026/2025', '2025/2024', '2024/2023', '2023/2022', '2025', '2024', '2023', '2022']
+selected_season = st.sidebar.selectbox('Season", ['2025/2024','2024/2023','2023/2022']
 
-#WebScraping Football Data
+# WebScraping Football Data
 
 def load_data(league, season):
   url = "https://www.football-data.co.uk/mmz4281/"+season+"/"+league+".csv"
