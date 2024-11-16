fec6500c238524515d98c4ff3981d488c1236472
myriiioliveira <myriiioliveira@gmail.com>
2024-11-15 22:56:25 -0300

    Update app.py

--- App/app.py
+++ App/app.py
@@ -5,7 +5,7 @@ import numpy as np
 st.title("Web App Football Data")
 
 st.sidebar.header("Leagues")
-selected_league = st.sidebar.selectbox('League', ['England','Germany','Italy','Spain','France','Scotland','Netherlands','Belgium','Portugal','Turkey','Greece','Argentina','Austria','Brazil','China','Denmark','Finland','Ireland','Japan','Mexico','Norway','Poland','Romania','Russia','Sweden','Switzerland','USA'])
+selected_league = st.sidebar.selectbox('League', ['England','Germany','Italy','Spain','France','Scotland','Netherlands','Belgium','Portugal','Turkey','Greece'])
 
 st.sidebar.header("Season")
 selected_season = st.sidebar.selectbox('Season', ['2025/2024','2024/2023','2023/2022'])
@@ -35,38 +35,7 @@ def load_data(league, season):
      league = 'T1'
   if selected_league == 'Greece':
      league = 'G1'
-  if selected_league == 'Argentina':
-     league = 'ARG'
-  if selected_league == 'Austria':
-     league = 'AUT'
-  if selected_league == 'Brazil':
-     league = 'BRA'
-  if selected_league == 'China':
-     league = 'CHN'
-  if selected_league == 'Denmark':
-     league = 'DNK'
-  if selected_league == 'Finland':
-     league = 'FIN'
-  if selected_league == 'Ireland':
-     league = 'IRL'
-  if selected_league == 'Japan':
-     league = 'JPN'
-  if selected_league == 'Mexico':
-     league = 'MEX'
-  if selected_league == 'Norway':
-     league = 'NOR'
-  if selected_league == 'Poland':
-     league = 'POL'
-  if selected_league == 'Romania':
-     league = 'ROU'
-  if selected_league == 'Russia':
-     league = 'RUS'
-  if selected_league == 'Sweden':
-     league = 'SWE'
-  if selected_league == 'Switzerland':
-     league = 'SWZ'
-  if selected_league == 'USA':
-     league = 'USA'
+
 
   if selected_season == '2024/2025':
      season = '2025'
@@ -78,10 +47,6 @@ def load_data(league, season):
      season = '2022'
 
   url = "https://www.football-data.co.uk/mmz4281/"+season+"/"+league+".csv"
-  data = pd.read_csv(url)
-  return data
-
-  url = "https://www.football-data.co.uk/new/"+league+".csv"
   data = pd.read_csv(url)
   return data
 
